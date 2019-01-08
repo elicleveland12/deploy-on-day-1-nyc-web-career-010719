2b2144bb57c37bf7d9dd3ec7414d0eab8ec7d731
Eli Cleveland <elicleveland12@gmail.com>
2019-01-08 14:23:22 +0000

    Automatically backed up by Learn

--- nonsense.rb
+++ nonsense.rb
@@ -2,9 +2,13 @@ require 'pry'
 
 # array = ["apple", "pear", "face", "champagne", "palm tree", "aardvark", "pineapple"]
 
-def word_count(sentence)
-  sentence.split(" ").size
-end
+# def word_count(sentence)
+#   sentence.split(" ").size
+# end
 
+def rude_greeting(name=nil)
+ name ||= "you jerk"
+ puts "Hey there, #{name}"
+end
 
 binding.pry
