8faa8d219e3cbd4351eda9b4a10730ace852101d
Eli Cleveland <elicleveland12@gmail.com>
2019-01-08 14:20:58 +0000

    Automatically backed up by Learn

--- nonsense.rb
+++ nonsense.rb
@@ -1,6 +1,6 @@
 require 'pry'
 
-array = ["apple", "pear", "face", "champagne", "palm tree", "aardvark", "pineapple"]
+# array = ["apple", "pear", "face", "champagne", "palm tree", "aardvark", "pineapple"]
 
 def word_count(sentence)
   sentence.split(" ").size
