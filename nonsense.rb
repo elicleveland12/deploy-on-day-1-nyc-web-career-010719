f9c953cfe64428fb7b5486ad2dab6a42e84811bb
Eli Cleveland <elicleveland12@gmail.com>
2019-01-08 14:20:41 +0000

    Automatically backed up by Learn

--- nonsense.rb
+++ nonsense.rb
@@ -3,5 +3,7 @@ require 'pry'
 array = ["apple", "pear", "face", "champagne", "palm tree", "aardvark", "pineapple"]
 
 def word_count(sentence)
-  sentence.split(" ").count
+  sentence.split(" ").size
 end
+
+word_count("The quick brown fox")
